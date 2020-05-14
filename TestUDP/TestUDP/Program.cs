c5377c6bc719ac7de98edc68602fe2e14e9a90b8
Mind Kafuu <chanagan_mind@msn.com>
2020-05-14 12:28:06 +0700

    UDP

---
+++ TestUDP/TestUDP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestUDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread udp = new Thread(CallUDP);
+            udp.Start();
+        }
+
+        static void CallUDP()
+        {
+            //Creates a UdpClient for reading incoming data.
+            UdpClient receivingUdpClient = new UdpClient(5006);
+
+            //Creates an IPEndPoint to record the IP Address and port number of the sender.
+            // The IPEndPoint will allow you to read datagrams sent from any source.
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            try
+            {
+
+                while (true)
+                {
+                    // Blocks until a message returns on this socket from a remote host.
+                    Byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+                    string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+                    //Console.WriteLine($" {Encoding.ASCII.GetString(receiveBytes, 0, receiveBytes.Length)}");
+                    Console.WriteLine("This is the message you received " +
+                                              returnData.ToString());
+                    Console.WriteLine("This message was sent from " +
+                                                RemoteIpEndPoint.Address.ToString() +
+                                                " on their port number " +
+                                                RemoteIpEndPoint.Port.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
